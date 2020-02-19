f9ac6fbcfa074b121f489715409b189382ab8fc1
Viktoriya Dzendzel <viktoriya.dzendzel@mindera.com>
2020-02-19 23:35:24 +0000

    test(NA): Initial commit

---
+++ src/main/test/java/com.devexperts/BaseTest.java
@@ -0,0 +1,13 @@
+package com.devexperts;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BaseTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
